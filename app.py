f459ca0fa7e5493e031f7fc2ac89c5d71caa10e1
karanongithub <karandeepsingh3451@gmail.com>
2024-04-01 22:00:00 +0530

    some padding and formatting req

--- app.py
+++ app.py
@@ -2,20 +2,26 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
+
 APP_TITLE = 'Electoral Bonds Dashboard'
 APP_SUBTITLE = 'Source: State Bank of India'
+
+with open("styles.css") as f:
+    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
 
 def main():
 
-    st.set_page_config(APP_TITLE)
     st.title(APP_TITLE)
     st.caption(APP_SUBTITLE)
 
+    img = st.image(
+        'CCI_UDHindu_KSL_URO5O15EU_R1555596349_0_cec15fbe-6287-4ffa-80d2-58a64c787a79.jpg', use_column_width=True)
+
     df = pd.read_csv('bonds.csv')
 
-    st.markdown('### Distribution of Bonds $')
-    st.caption('Among Parties')
+    dist = st.markdown('### Distribution of Bonds ₹')
+    # cap = st.caption('Among Parties')
 
 # The Tree Map Visulation
     fig = px.treemap(df, path=[px.Constant('All Parties'), 'pol_party'], values='price',
@@ -27,14 +33,37 @@ def main():
     fig.update_traces(root_color="lightgrey")
     st.plotly_chart(figure_or_data=fig, use_container_width=True)
 
-    # write(df[df['pol_party'] == sel_party].head())
 
 # Side bar
 
-    side_sel_party = st.sidebar.selectbox(
-        "Select the Political Party", options=df.pol_party.unique())
+    unique = df.pol_party.unique()
+    side_sel_party = st.sidebar.radio(
+        "Select the Political Party", options=unique)
 
-    st.write(df[df['pol_party'] == side_sel_party])
+    df = df[df['pol_party'] == side_sel_party]
+    metric_total = df['price'].sum()
+    avg_total = metric_total/df.price.count()
+
+    st.write(f'{side_sel_party}')
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.metric(
+            label='Total Amount of Bonds Purchased in ₹', value="{:,}".format(metric_total))
+    with col3:
+        st.metric(label='Avg Amount in ₹',
+                  value="{:,}".format(round(avg_total)))
+
+    st.markdown('### Data in CSV')
+    st.dataframe(df,
+                 column_config={
+                     "encashment_date": "Date of Cashing",
+                     "pol_party": "Political Party",
+                     "purchased_date": "Date of Purchasing",
+                     "purchaser_name": "Purchaser Name",
+                     "price": "Price"
+                 }, use_container_width=True)
 
 
 if __name__ == '__main__':
